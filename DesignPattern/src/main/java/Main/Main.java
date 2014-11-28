3e3363371fb9c81bb8c12307817d84a6613f3b98
Petrovick <you@example.com>
2014-11-28 00:04:34 -0200

    Main refactoring

--- DesignPattern/src/main/java/Main/Main.java
+++ DesignPattern/src/main/java/Main/Main.java
@@ -3,6 +3,7 @@ import Template.Bicicleta;
 import Template.BicicletaInexistenteException;
 import Factory.FabricaBicicleta;
 import Strategy.Transacao;
+import Strategy.TransacaoPrazo;
 import Strategy.TransacaoVista;
 import singleton.Caixa;
 
@@ -17,6 +18,38 @@ public class Main
             Bicicleta b = new FabricaBicicleta().fabricarBicicleta("rampa");
             Transacao transacao = new Transacao();
             transacao.setTipoTransacao(new TransacaoVista());
+            transacao.realizarTransacao(b.getValor());
+            System.out.println(Caixa.getValorCaixa());
+            System.out.println("Construindo bicicleta");
+        }
+        catch(BicicletaInexistenteException ex)
+        {
+            System.out.println(ex);
+        }
+        
+        System.out.println("--------------------------");
+        
+        try
+        {
+            Bicicleta b = new FabricaBicicleta().fabricarBicicleta("corrida");
+            Transacao transacao = new Transacao();
+            transacao.setTipoTransacao(new TransacaoPrazo());
+            transacao.realizarTransacao(b.getValor());
+            System.out.println(Caixa.getValorCaixa());
+            System.out.println("Construindo bicicleta");
+        }
+        catch(BicicletaInexistenteException ex)
+        {
+            System.out.println(ex);
+        }
+        
+        System.out.println("--------------------------");
+        
+        try
+        {
+            Bicicleta b = new FabricaBicicleta().fabricarBicicleta("naoexiste");
+            Transacao transacao = new Transacao();
+            transacao.setTipoTransacao(new TransacaoPrazo());
             transacao.realizarTransacao(b.getValor());
             System.out.println(Caixa.getValorCaixa());
             System.out.println("Construindo bicicleta");
